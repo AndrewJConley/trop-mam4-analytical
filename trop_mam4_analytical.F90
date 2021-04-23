19cf3add811d62db02a81838065070aae69bf1d1
AndrewJConley <aconley@ucar.edu>
2021-04-22 18:59:56 -0600

    analytical solution ; needs rate constants

--- trop_mam4_analytical.F90
+++ trop_mam4_analytical.F90
@@ -1,5 +1,7 @@
-      module trop_mam4_analytical
+module trop_mam4_analytical
+
 !-------------------------------------------------------
+!  analytical solution of the following system
 !  k0  [jh2o2]       H2O2 + hv ->
 !  k1  [usr_HO2_HO2] HO2 + HO2 -> H2O2
 !  k2                H2O2 + OH -> H2O + HO2                                           ; 2.9e-12, -160
@@ -8,7 +10,7 @@
 !  k5  [usr_DMS_OH]  DMS + OH -> .5 * SO2 + .5 * HO2
 !  k6                DMS + NO3 -> SO2 + HNO3                                          ; 1.9e-13,  520.
 !
-!  gas_array = 'H2O2', HO2, SO2, H2SO4, DMS, NO3, HNO3
+!  Fixed (constant) values for HO2, OH, NO3
 !-------------------------------------------------------
 
       use shr_kind_mod, only : r8 => shr_kind_r8
@@ -19,56 +21,78 @@
 
       contains
 
-      subroutine set_rates(temperature, pressure, rate_constant )
-
+      subroutine time_advance( time_step_seconds, temperature, pressure, &
+        j_h2o2, &
+        HO2, OH, NO3, &
+        H2O2_t, H2O2_0, &
+        SO2_t, SO2_0, &
+        H2SO4_t, H2SO4_0, &
+        DMS_t, DMS_0, &
+        HNO3_t, HNO3_0 &
+        )
       implicit none
 
 !-------------------------------------------------------
 !       ... dummy arguments
 !-------------------------------------------------------
-      real(r8), intent(in)    :: temperature
-      real(r8), intent(in)    :: pressure
-      real(r8), intent(out) :: rate_constant(0:6)
-
-!-------------------------------------------------------
-!       ... local variables
-!-------------------------------------------------------
-      itemp = 1/temperature
-      (0,1,3,5) are so far undefined, but need to be added here?
-      rate_constant(2) = 2.9e-12_r8 * exp( -160._r8 * itemp(:,:) )
-      rate_constant(4) = 9.6e-12_r8 * exp( -234._r8 * itemp(:,:) )
-      rate_constant(6) = 1.9e-13_r8 * exp( 520._r8 * itemp(:,:) )
-
-      end subroutine setrxt
-
-
-      subroutine time_advance( rate_constant, gas_array )
-
-      implicit none
-
-!-------------------------------------------------------
-!       ... dummy arguments
-!-------------------------------------------------------
-      real(r8), intent(in)    :: rate_constant
-      real(r8), intent(inout) :: gas_array
       real(r8), intent(in)    :: time_step_seconds
+      real(r8), intent(in)    :: j_h2o2         ! photodecomp rate of H2O2 -> 2*OH
+      real(r8), intent(in)    :: HO2, OH, NO3   ! fixed values
+      real(r8), intent(in)    :: H2O2_0, SO2_0, H2SO4_0, DMS_0, HNO3_0 ! initial value
+      real(r8), intent(out)   :: H2O2_t, SO2_t, H2SO4_t, DMS_t, HNO3_t ! final value
+      real(r8) :: rate_constant(0:6)
+
 
 !-------------------------------------------------------
 !       ... local variables
 !-------------------------------------------------------
 
-!  gas_array = 'H2O2', HO2, SO2, H2SO4, DMS, NO3, HNO3
+      itemp = 1/temperature
+      (1,3,5) are so far undefined, but need to be added here?
+      rate_constant(0) = j_h2o2
+      rate_constant(1) = 
+      rate_constant(2) = 2.9e-12_r8 * exp( -160._r8 * itemp)
+      rate_constant(3) = 
+      rate_constant(4) = 9.6e-12_r8 * exp( -234._r8 * itemp)
+      rate_constant(5) = 
+      rate_constant(6) = 1.9e-13_r8 * exp( 520._r8 * itemp)
+
+
       ! DMS(t) given DMS_0 = DMS(t=0)
-      c = -rate_constant(4)* OH - rate_constant(5)*OH-rate_constant(6)
-      DMS = DMS_0 * exp( c * time_step_seconds)
+      C = -rate_constant(4)* OH - rate_constant(5)*OH-rate_constant(6)
+      DMS = DMS_0 * exp( C * time_step_seconds)
 
       ! SO2
-      alpha = -rate_constant(3) * OH
-      beta = rate_constant(4) + 0.5*rate_constant(5)*OH + rate_constant(6)*NO3
-      ratio = beta/(c-alpha)
-      coeff_1 = (SO2_0 - ratio) exp(alpha*time_step_seconds) + ratio*exp(c * time_step_seconds)
-      
+      A = -rate_constant(3) * OH
+      B = rate_constant(4) + 0.5*rate_constant(5)*OH + rate_constant(6)*NO3
+      beta = B * DMS_0
+      alpha = A
+      gamma = C
+      SO2_t = SO2_0 &
+        + beta * ( exp(gamma*time_step_seconds) - exp( alpha * time_step_seconds ) &
+          / (gamma - alpha )
+
+      ! H2SO4
+      kappa = rate_constant(3) * OH
+      H2SO4_t = H2SO4_0 &
+        + kappa * SO2_0 * time_step_seconds &
+        + (beta / (gamma - alpha) ) * ( &
+             ( exp( gamma * time_step_seconds ) - 1) / gamma &
+            -(exp( alpha * time_step_seconds ) - 1) / alpha &
+          )
+
+      ! HNO3
+      eta = rate_constant(6) * OH
+      HNO3_t = HNO3_0 &
+        + eta * DMS_0 * ( (exp(gamma*time_step_seconds) - 1) / gamma )
+   
+      ! H2O2
+      zeta = - rate_constant(0) - rate_constant(2) * OH
+      phi = rate_constant(1) * HO2 * HO2
+      H2O2_t = (H2O2_0 + phi/zeta ) *exp(zeta * time_step_seconds) &
+        - phi/zeta
+
 
       end subroutine time_advance
 
-      end module trop_mam4_analytical
+end module trop_mam4_analytical
